2ec15b75d534666abb8c58e298f0d65f942b5f7c
KroNton <kyrlosfekry@gmail.com>
2022-07-13 15:46:41 +0200

    address of variable ex.

---
+++ pointers/var_addr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//addresses of variables
+
+
+
+int main()
+{
+    /* code */
+int var1=11;
+int var2=22;
+int var3=33;
+
+cout<< &var1 <<endl
+    << &var2 <<endl
+    << &var3 <<endl;
+
+    return 0;
+}
