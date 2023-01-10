ae664efe91ab85c6ac58d240adf98b5aefe5696e
KroNton <kyrlosfekry@gmail.com>
2023-01-10 21:12:32 +0200

    solve data type prob.

---
+++ week1/b_data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+int int_value;
+long long long_value;
+char char_value;
+float float_value;
+double double_value;
+
+cin >>int_value>>long_value>>char_value>>float_value>>double_value;
+cout <<int_value<<endl<<long_value<<endl<<char_value<<endl<<float_value<<endl<<double_value<<endl;
+
+return 0;
+}
