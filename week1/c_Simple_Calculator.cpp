cae390c7841291a87f9748621bcafd9c1e5d7bf5
KroNton <kyrlosfekry@gmail.com>
2023-01-10 21:29:11 +0200

    solve simple calculator prob.

---
+++ week1/c_Simple_Calculator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+
+long long x_value ,y_vlaue;
+cin >>x_value>>y_vlaue;
+
+cout <<x_value<<" + "<<y_vlaue<<" = "<<x_value+y_vlaue<<endl;
+cout <<x_value<<" * "<<y_vlaue<<" = "<<x_value*y_vlaue<<endl;
+cout <<x_value<<" - "<<y_vlaue<<" = "<<x_value-y_vlaue<<endl;
+
+
+
+return 0;
+}
