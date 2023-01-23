66483e80478671707df49844ecb2f3e1db2a73cd
KroNton <kyrlosfekry@gmail.com>
2023-01-23 04:15:04 +0200

    Multiplication table prob.

---
+++ week2/f_Multiplication_table.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int num ;
+
+cin >> num ;
+
+for (size_t i = 1; i <= 12; i++)
+{
+
+cout<<num<<" * "<<i<<" = "<<num*i<<endl  ;
+}
+
+
+
+
+
+
+
+
+return 0;
+}
