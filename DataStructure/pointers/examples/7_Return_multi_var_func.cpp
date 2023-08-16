9bec8365a4e45f9a694e37fdaff1fbfc4159d83d
KroNton <kyrlosfekry@gmail.com>
2023-08-17 02:13:31 +0300

    update the code

--- DataStructure/pointers/examples/7_Return_multi_var_func.cpp
+++ DataStructure/pointers/examples/7_Return_multi_var_func.cpp
@@ -3,35 +3,35 @@
 
 
 void getMinAndMax(int numbers[],int size,int*min,int*max){
-double temp;
 
-for (size_t i = 0; i < size; i++)
-{
- if (numbers[i]<*min)
- {
-    *min=numbers[i];
- }
- if (numbers[i]>*max)
- {
-    *max=numbers[i];
- }
- 
-}
+
+   for (size_t i = 0; i < size; i++)
+   {
+      if (numbers[i]<*min)
+      {
+         *min=numbers[i];
+      }
+      if (numbers[i]>*max)
+      {
+         *max=numbers[i];
+      }
+      
+   }
 
 }
 
 int main()
 {
 
-int numbers[5]={12,-5,-12,20,18};
-int min=numbers[0];
-int max=numbers[0];
+   int numbers[5]={12,-5,-12,20,18};
+   int min=numbers[0];
+   int max=numbers[0];
 
-getMinAndMax(numbers,5,&min,&max);
+   getMinAndMax(numbers,5,&min,&max);
 
-std::cout<<"min number: "<<min<<std::endl;
-std::cout<<"max number: "<<max<<std::endl;
+   std::cout<<"min number: "<<min<<std::endl;
+   std::cout<<"max number: "<<max<<std::endl;
 
-return 0;
+   return 0;
 }
 
