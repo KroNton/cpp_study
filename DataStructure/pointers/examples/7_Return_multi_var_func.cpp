d13b400954e01ae8f43297a1c6c1d06ba0042287
KroNton <kyrlosfekry@gmail.com>
2023-08-17 02:12:15 +0300

    return multipule values from a function

---
+++ DataStructure/pointers/examples/7_Return_multi_var_func.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+
+
+void getMinAndMax(int numbers[],int size,int*min,int*max){
+double temp;
+
+for (size_t i = 0; i < size; i++)
+{
+ if (numbers[i]<*min)
+ {
+    *min=numbers[i];
+ }
+ if (numbers[i]>*max)
+ {
+    *max=numbers[i];
+ }
+ 
+}
+
+}
+
+int main()
+{
+
+int numbers[5]={12,-5,-12,20,18};
+int min=numbers[0];
+int max=numbers[0];
+
+getMinAndMax(numbers,5,&min,&max);
+
+std::cout<<"min number: "<<min<<std::endl;
+std::cout<<"max number: "<<max<<std::endl;
+
+return 0;
+}
+
